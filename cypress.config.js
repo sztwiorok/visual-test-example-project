771dd4257ca4eacdb7fcb546e37b515d79e8b2a1
bartoszwrobel <bartoszwrobel591@gmail.com>
2023-12-08 13:38:20 +0100

    Fix

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require("cypress");
 
+const baseUrl = process.env.CI ? "http://0.0.0.0:4000" : "http://localhost:4000";
+
 module.exports = defineConfig({
   video: false,
   e2e: {
     setupNodeEvents(on, config) {},
+    baseUrl,
   },
 });
--- cypress/e2e/snapshotTest.cy.js
+++ cypress/e2e/snapshotTest.cy.js
@@ -1,8 +1,6 @@
-const url = process.env.CI ? "http://0.0.0.0:4000" : "http://localhost:4000";
-
 describe("snapshot example", () => {
   it("Take snapshot 1", () => {
-    cy.visit(url);
+    cy.visit('/');
     new Array(1).fill("").forEach((_, index) => {
       cy.takeSnap(`Snapshot ${index}`);
     });
