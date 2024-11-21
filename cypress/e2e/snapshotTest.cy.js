f507c30e176a3d99f32f0d61df68f759945cddfc
bartoszwrobel <bartoszwrobel591@gmail.com>
2024-11-21 13:57:39 +0100

    Added nested snapshots

--- cypress/e2e/snapshotTest.cy.js
+++ cypress/e2e/snapshotTest.cy.js
@@ -1,8 +1,30 @@
 describe("snapshot example", () => {
   it("Take snapshot 1", () => {
     cy.visit('/');
-    new Array(10).fill("").forEach((_, index) => {
-      cy.takeSnap(`Snapshot ${index}`);
+    new Array(1).fill("").forEach((_, index_2) => {
+      cy.takeSnap(`Snapshot${index_2}`);
+    });
+    new Array(1).fill("").forEach((_, index_1) => {
+      new Array(2).fill("").forEach((_, index_2) => {
+        cy.takeSnap(`Group ${index_1}/Snapshot${index_2}`);
+      });
+    });
+    new Array(1).fill("").forEach((_, index_1) => {
+      new Array(2).fill("").forEach((_, index_2) => {
+        new Array(2).fill("").forEach((_, index_3) => {
+          cy.takeSnap(`Group ${index_1}/Group ${index_2}/Snapshot${index_3}`);
+        });
+      });
+    });
+
+    new Array(1).fill("").forEach((_, index_1) => {
+      new Array(2).fill("").forEach((_, index_2) => {
+        new Array(2).fill("").forEach((_, index_3) => {
+          new Array(2).fill("").forEach((_, index_4) => {
+            cy.takeSnap(`Group ${index_1}/Group ${index_2}/Group ${index_3}/Snapshot${index_4}`);
+          });
+        });
+      });
     });
   });
   //
